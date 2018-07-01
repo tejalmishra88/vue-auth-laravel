7cf1583c524c23dddbb7b34ba46f776c32b86f55
tejalmishra88 <tejalmishra88@gmail.com>
2018-07-01 18:09:40 +1000

    routes in api.php

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ Route::post('authenticate', 'AuthenticateController@authenticate');
 Route::group(['middleware' => 'jwt.auth'], function()
 {
     Route::get('user', 'UserController@show');
-    Route::get('user', 'UserController@show');
+    Route::get('task', 'tasks1controller@getnames');
     Route::post('user/profile/update', 'UserController@updateProfile');
     Route::post('user/password/update', 'UserController@updatePassword');
 });
